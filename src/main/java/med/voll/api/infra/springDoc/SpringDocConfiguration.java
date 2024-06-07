b9557be9a3b0352d6955e1922f2ac1903e93156c
jonas mendes <jonas.jsm2903@gmail.com>
2024-06-07 14:28:58 -0300

    configurando testes

--- src/main/java/med/voll/api/infra/springDoc/SpringDocConfiguration.java
+++ src/main/java/med/voll/api/infra/springDoc/SpringDocConfiguration.java
@@ -3,12 +3,10 @@ package med.voll.api.infra.springDoc;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
+
 
 @Configuration
 public class SpringDocConfiguration {
